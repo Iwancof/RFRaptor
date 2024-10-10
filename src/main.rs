2c0c3a909eb99079e3ce12b7835d792492af36ec
Iwancof <rock.iwancof@gmail.com>
2024-10-11 01:44:10 +0900

    とりあえず全部移植してみたが、動かず

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(iter_array_chunks)]
+#![allow(internal_features)]
 #![feature(core_intrinsics)]
 #![feature(array_chunks)]
+#![feature(let_chains)]
 
 use core::fmt;
 
@@ -13,6 +15,7 @@ use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
 #[log_derive::logfn(ok = "TRACE", err = "ERROR")]
+// #[tokio::main]
 fn main() -> anyhow::Result<()> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
     soapysdr::configure_logging();
@@ -31,16 +34,17 @@ fn main() -> anyhow::Result<()> {
 
     let config = SDRConfig {
         channels: 0,
-        center_freq: 2426.0e6, // bluetooth
+        center_freq: 2441.0e6, // bluetooth
         sample_rate: 20.0e6,
         bandwidth: 20.0e6,
-        gain: 20.0,
+        gain: 32.,
     };
 
     log::info!("config = {}", config);
     config.set(&dev)?;
 
     let mut magic: MaybeUninit<ice9_bindings::pfbch2_t> = MaybeUninit::uninit();
+    let mut fsk: MaybeUninit<ice9_bindings::fsk_demod_t> = MaybeUninit::uninit();
 
     // initialize ice9 bindings
     unsafe {
@@ -67,10 +71,15 @@ fn main() -> anyhow::Result<()> {
             m as _,
             buffer.as_mut_ptr(),
         );
+
+        fsk_demod_init(fsk.as_mut_ptr());
     }
 
     let mut magic = unsafe { magic.assume_init() };
     println!("{:?}", magic);
+
+    let mut fsk = unsafe { fsk.assume_init() };
+    println!("{:?}", fsk);
 
     let mut stream = dev.rx_stream::<num_complex::Complex<i8>>(&[config.channels])?;
     // let mut _write_stream = dev.tx_stream::<num_complex::Complex<u8>>(&[config.channels])?;
@@ -78,25 +87,30 @@ fn main() -> anyhow::Result<()> {
     let sb = signalbool::SignalBool::new(&[signalbool::Signal::SIGINT], signalbool::Flag::Restart)?;
 
     let mut buffer = vec![num_complex::Complex::<i8>::new(0, 0); stream.mtu()?];
-    let mut burst = CRCF::new();
+    let mut burst = (0..20)
+        .into_iter()
+        .map(|_| Burst::new())
+        .collect::<Vec<_>>();
 
     stream.activate(None)?;
 
     const BATCH_SIZE: usize = 4096;
 
-    use std::io::{BufWriter, Write};
-    let mut file = BufWriter::new(std::fs::File::create("signal.dat")?);
+    // use std::io::{BufWriter, Write};
+    // let mut file = BufWriter::new(std::fs::File::create("signal.dat")?);
 
-    let mut counter = 0;
+    let mut planner = rustfft::FftPlanner::new();
+
+    let mut fft_in_buffer = vec![Vec::with_capacity(BATCH_SIZE); 20];
+    let mut output = vec![0i16; 96 * 2];
+
+    create_catcher_threads();
 
     '_outer: for _ in 0.. {
         let read = stream.read(&mut [&mut buffer[..]], 1_000_000)?;
         assert_eq!(read, buffer.len());
 
-        let mut channel_0 = Vec::with_capacity(BATCH_SIZE);
         for chunk in buffer.chunks_mut(20 / 2) {
-            let mut output = vec![0i16; 96 * 2];
-
             unsafe {
                 // SAFETY: Complex<T> has `repr(C)` layout
                 let flat_chunk = chunk.as_mut_ptr() as *mut i8;
@@ -108,94 +122,71 @@ fn main() -> anyhow::Result<()> {
                 );
             }
 
-            let buf = output[..20 * 2]
+            output[..20 * 2]
                 .array_chunks::<2>()
-                .map(|reim_pair| {
-                    let [re, im] = reim_pair;
-
+                .enumerate()
+                .for_each(|(i, [re, im])| {
                     let re = *re as f32 / 32768.0;
                     let im = *im as f32 / 32768.0;
 
-                    num_complex::Complex::new(re, im)
-                })
-                .collect::<Vec<_>>();
+                    let signal = num_complex::Complex::new(re, im);
 
-            channel_0.push(buf[9]);
+                    fft_in_buffer[i].push(signal);
+                });
 
-            if channel_0.len() == BATCH_SIZE {
-                let mut planner = rustfft::FftPlanner::new();
-                let fft = planner.plan_fft_inverse(4096);
+            for (i, fft_in) in (&mut fft_in_buffer[..20]).into_iter().enumerate() {
+                if fft_in.len() == BATCH_SIZE {
+                    let fft = planner.plan_fft_inverse(4096);
 
-                fft.process(&mut channel_0);
+                    fft.process(fft_in);
 
-                for e in &channel_0 {
-                    // match burst.catcher(*e / 20 as f32) as _ {
-                    let (sample, status) = burst.execute(*e / 20 as f32);
-                    match status as _ {
-                        //liquid_dsp_bindings_sys::agc_squelch_mode_LIQUID_AGC_SQUELCH_RISE => {
-                        //    // println!("LIQUID_AGC_SQUELCH_RISE: {:?}", sample);
-                        //}
-                        liquid_dsp_bindings_sys::agc_squelch_mode_LIQUID_AGC_SQUELCH_SIGNALHI => {
-                            // println!("LIQUID_AGC_SQUELCH_SIGNALHI");
-                            writeln!(file, "{} {} {}", counter, sample.norm(), 0)?;
-                        }
-                        //liquid_dsp_bindings_sys::agc_squelch_mode_LIQUID_AGC_SQUELCH_TIMEOUT => {
-                        //    // println!("LIQUID_AGC_SQUELCH_TIMEOUT");
-                        //}
-                        _ => {
-                            writeln!(file, "{} {} {}", counter, sample.norm(), 1)?;
-                        }
-                    }
+                    FFT_SIGNAL_CHANNEL[i]
+                        .lock()
+                        .unwrap()
+                        .extend_from_slice(fft_in);
 
-                    counter += 1;
+                    /*
+                                        for e in &*fft_in {
+                                            if let Some(mut packet) = burst[i].catcher(*e / 20 as f32)
+                                                && 132 <= packet.data.len()
+                                            {
+                                                // log::info!("packet {}. timestamp: {}", packet.data.len(), packet.timestamp);
+
+                                                unsafe {
+                                                    use ice9_bindings::*;
+
+                                                    let mut out = MaybeUninit::zeroed();
+                                                    fsk_demod(
+                                                        &mut fsk as _,
+                                                        packet.data.as_mut_ptr() as _,
+                                                        packet.data.len() as _,
+                                                        out.as_mut_ptr(),
+                                                    );
+
+                                                    let out = out.assume_init();
+
+                                                    if !out.demod.is_null() && !out.bits.is_null() {
+                                                        // println!("found: {:?}", out);
+                                                        let slice =
+                                                            std::slice::from_raw_parts(out.bits, out.bits_len as usize);
+                                                        println!(
+                                                            "idx = {}, {} {} {:?}",
+                                                            i,
+                                                            packet.timestamp,
+                                                            slice.len(),
+                                                            &slice[..40]
+                                                        );
+                                                    }
+                                                }
+                                            }
+                                        }
+                    */
                 }
-
-                /*
-                use std::io::{BufWriter, Write};
-
-                let mut file = BufWriter::new(std::fs::File::create("output.dat")?);
-
-                // translate this python code `np.abs(np.fft.fft(x))**2 / (N*Fs)`
-                let power = channel_0.iter().map(|x| x.norm_sqr()).collect::<Vec<_>>();
-                let power = power
-                    .iter()
-                    .map(|x| x / (BATCH_SIZE as f32 * config.sample_rate as f32))
-                    .collect::<Vec<_>>();
-
-                // make it log scale
-                let power = power.iter().map(|x| 10.0 * x.log10()).collect::<Vec<_>>();
-
-                let freq_step = config.sample_rate / BATCH_SIZE as f64;
-
-                // shift the zero frequency to the center
-                let mut power = power
-                    .iter()
-                    .enumerate()
-                    .map(|(i, x)| {
-                        let i = if i < BATCH_SIZE / 2 {
-                            i as isize
-                        } else {
-                            i as isize - BATCH_SIZE as isize
-                        };
-                        (i, x)
-                    })
-                    .collect::<Vec<_>>();
-
-                // sort by frequency
-                power.sort_by(|a, b| a.0.cmp(&b.0));
-
-                // convert to frequency
-                let power = power
-                    .iter()
-                    .map(|(i, x)| (*i as f64 * freq_step, *x))
-                    .collect::<Vec<_>>();
-
-                for p in power.iter() {
-                    writeln!(file, "{} {}", p.0, p.1)?;
+            }
+            for (i, fft_in) in (&mut fft_in_buffer).into_iter().enumerate() {
+                if fft_in.len() == BATCH_SIZE {
+                    fft_in.clear();
                 }
-                */
-
-                // break '_outer;
             }
         }
 
@@ -239,6 +230,7 @@ impl fmt::Display for SDRConfig {
     }
 }
 
+#[derive(Debug)]
 struct CRCF {
     crcf: liquid_dsp_bindings_sys::agc_crcf,
 }
@@ -278,7 +270,8 @@ impl CRCF {
     pub fn status(&self) -> SquelchStatus {
         SquelchStatus::from_i32(unsafe {
             liquid_dsp_bindings_sys::agc_crcf_squelch_get_status(self.crcf)
-        }).unwrap()
+        })
+        .unwrap()
     }
 }
 
@@ -288,6 +281,7 @@ impl Drop for CRCF {
     }
 }
 
+#[derive(Debug)]
 struct Burst {
     crcf: CRCF,
     in_burst: bool,
@@ -308,6 +302,7 @@ pub enum SquelchStatus {
 
 use chrono::prelude::*;
 
+#[derive(Debug)]
 struct Packet {
     data: Vec<num_complex::Complex<f32>>,
     timestamp: DateTime<Utc>,
@@ -322,6 +317,7 @@ impl Burst {
         }
     }
 
+    #[allow(unused)]
     pub fn catcher(&mut self, signal: num_complex::Complex<f32>) -> Option<Packet> {
         let (signal, status) = self.crcf.execute(signal);
 
@@ -336,7 +332,6 @@ impl Burst {
             SquelchStatus::Timeout => {
                 self.in_burst = false;
                 let data = self.burst.clone();
-                self.burst.clear();
 
                 return Some(Packet {
                     data,
@@ -347,5 +342,109 @@ impl Burst {
         }
 
         None
+    }
+}
+
+unsafe impl Send for Burst {}
+
+static FFT_SIGNAL_CHANNEL: [std::sync::LazyLock<
+    std::sync::Arc<std::sync::Mutex<Vec<num_complex::Complex<f32>>>>,
+>; 20] = [const { std::sync::LazyLock::new(|| std::sync::Arc::new(std::sync::Mutex::new(Vec::new()))) };
+    20];
+
+fn create_catcher_threads() {
+    for i in 0..20 {
+        std::thread::spawn(move || {
+            let mut burst = Burst::new();
+            let mut fsk: MaybeUninit<ice9_bindings::fsk_demod_t> = MaybeUninit::uninit();
+
+            unsafe {
+                use ice9_bindings::*;
+
+                fsk_demod_init(fsk.as_mut_ptr());
+            }
+
+            let mut fsk = unsafe { fsk.assume_init() };
+
+            let mut tmp = Vec::with_capacity(4096);
+            loop {
+                core::mem::swap(&mut *FFT_SIGNAL_CHANNEL[i].lock().unwrap(), &mut tmp);
+
+                if tmp.len() == 0 {
+                    // tokio::time::sleep(tokio::time::Duration::from_millis(10));
+                    std::thread::sleep(std::time::Duration::from_millis(10));
+                    continue;
+                }
+
+                for s in &tmp {
+                    if let Some(mut packet) = burst.catcher(s / 20 as f32)
+                        && 132 <= packet.data.len()
+                    {
+                        /*
+                        log::info!(
+                            "packet {}. timestamp: {}. idx: {}",
+                            packet.data.len(),
+                            packet.timestamp,
+                            i
+                        );
+                        */
+
+                        unsafe {
+                            use ice9_bindings::*;
+
+                            let mut out = MaybeUninit::zeroed();
+                            fsk_demod(
+                                &mut fsk as _,
+                                packet.data.as_mut_ptr() as _,
+                                packet.data.len() as _,
+                                out.as_mut_ptr(),
+                            );
+
+                            let out = out.assume_init();
+
+                            if !out.demod.is_null() && !out.bits.is_null() {
+                                // println!("found: {:?}", out);
+                                let slice: &mut [u8] =
+                                    std::slice::from_raw_parts_mut(out.bits, out.bits_len as usize);
+                                /*
+                                                                println!(
+                                                                    "idx = {}, {} {} {:?}",
+                                                                    i,
+                                                                    packet.timestamp,
+                                                                    slice.len(),
+                                                                    &slice[..40]
+                                                                );
+                                */
+
+                                /*
+                                                                if &slice[..6] == &[0, 1, 0, 1, 0, 1] {
+                                                                    println!("found preamble");
+                                                                }
+                                */
+
+                                use ice9_bindings::*;
+
+                                let lap =
+                                    btbb_find_ac(slice.as_mut_ptr() as _, slice.len() as _, 1);
+                                // println!("lap = {:?}", lap);
+
+                                if lap != 0xffffffff {
+                                    let p = ble_easy(
+                                        slice.as_mut_ptr() as _,
+                                        slice.len() as _,
+                                        (2441 + if i < 10 { i } else { i - 20 }) as _,
+                                    );
+
+                                    println!("p = {:?}", *p);
+                                }
+                            }
+                        }
+                    }
+                }
+                // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+                tmp.clear();
+            }
+        });
     }
 }
