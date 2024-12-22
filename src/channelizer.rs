1866f61d6e4f250a8080d97ce1364b3248424fea
Iwancof <rock.iwancof@gmail.com>
2024-12-23 05:11:13 +0900

    add synthesizer(working!!) and tests

--- src/channelizer.rs
+++ src/channelizer.rs
@@ -62,6 +62,155 @@ impl Drop for Channelizer {
     }
 }
 
+impl core::fmt::Display for Channelizer {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        use wrcap::lent_stdout;
+
+        let ((), content) = lent_stdout().unwrap().capture_string(|| {
+            unsafe {
+                liquid_dsp_sys::firpfbch2_crcf_print(self.analyzer.as_ptr());
+            };
+        }).unwrap();
+
+        writeln!(f, "Channelizer")?;
+        writeln!(f, "- num_channels: {}", self.num_channels)?;
+        writeln!(f, "- analyser: {:p}", self.analyzer)?;
+
+        writeln!(f, "- firpfbch2_crcf_print")?;
+        write!(f, "  - {}", content.strip_suffix("\n").unwrap())?;
+
+        Ok(())
+    }
+}
+
 pub struct Synthesizer {
+    num_channels: usize,
+
     synthesizer: core::ptr::NonNull<liquid_dsp_sys::firpfbch2_crcf_s>,
+
+    #[doc(hidden)]
+    channel_half: usize,
+
+    #[doc(hidden)]
+    working_buffer: Vec<Complex<f32>>,
+    // len(working_buffer) = num_channels
+}
+
+impl Synthesizer {
+    pub fn new(num_channels: usize) -> Self {
+        let synthesizer = liquid_get_pointer(|| unsafe {
+            firpfbch2_crcf_create_kaiser(
+                LIQUID_SYNTHESIZER as i32,
+                num_channels as u32,
+                SYMBOL_DELAY,
+                60.0,
+            )
+        })
+        .expect("firpfbch2_crcf_create_kaiser failed (synthesizer)");
+
+        Self {
+            num_channels,
+            channel_half: num_channels / 2,
+            synthesizer,
+            working_buffer: vec![Complex::new(0.0, 0.0); num_channels / 2],
+        }
+    }
+
+    pub fn synthesizer(&mut self, input: &[Complex<f32>]) -> &[Complex<f32>] {
+        debug_assert_eq!(input.len(), self.num_channels);
+        debug_assert_eq!(self.working_buffer.len(), self.channel_half);
+
+        liquid_do_int(|| unsafe {
+            liquid_dsp_sys::firpfbch2_crcf_execute(
+                self.synthesizer.as_ptr(),
+                input.as_ptr() as *mut _,
+                self.working_buffer.as_mut_ptr(),
+            )
+        })
+        .expect("firpfbch2_crcf_execute failed");
+
+        &self.working_buffer
+    }
+}
+
+impl Drop for Synthesizer {
+    fn drop(&mut self) {
+        liquid_do_int(|| unsafe {
+            liquid_dsp_sys::firpfbch2_crcf_destroy(self.synthesizer.as_ptr())
+        })
+        .expect("firpfbch2_crcf_destroy failed (synthesizer)");
+    }
+}
+
+impl core::fmt::Display for Synthesizer {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        use wrcap::lent_stdout;
+
+        let ((), content) = lent_stdout().unwrap().capture_string(|| {
+            unsafe {
+                liquid_dsp_sys::firpfbch2_crcf_print(self.synthesizer.as_ptr());
+            };
+        }).unwrap();
+
+        writeln!(f, "Synthesizer")?;
+        writeln!(f, "- num_channels: {}", self.num_channels)?;
+        writeln!(f, "- synthesizer: {:p}", self.synthesizer)?;
+
+        writeln!(f, "- firpfbch2_crcf_print")?;
+        write!(f, "  - {}", content.strip_suffix("\n").unwrap())?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rand::prelude::*;
+
+    #[test]
+    fn uptest_random_data() {
+        let num_channels = 8;
+        let samples = num_channels * 100;
+
+        let mut channelizer = Channelizer::new(num_channels);
+        let mut synthesizer = Synthesizer::new(num_channels);
+
+        println!("{}", channelizer);
+        println!("{}", synthesizer);
+
+        let seed = 0;
+        let mut rng = SmallRng::seed_from_u64(seed);
+
+        let data = (0..samples).map(|_| Complex::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0))).collect::<Vec<_>>();
+        let mut synthesized = vec![];
+        
+        for chunk in data.chunks(num_channels / 2) {
+            let channelized = channelizer.channelize(chunk);
+            let syn = synthesizer.synthesizer(channelized);
+
+            synthesized.extend_from_slice(syn);
+        }
+
+        let delay = 2 * num_channels * SYMBOL_DELAY as usize - num_channels / 2 + 1;
+
+        let mut rmes = 0.0;
+        for i in 0..samples {
+            let compare = if i < delay {
+                Complex::new(0.0, 0.0)
+            } else {
+                data[i - delay]
+            };
+
+            println!("{}: {:?} == {:?}", i, synthesized[i], compare);
+            rmes += (synthesized[i] - compare).norm_sqr();
+        }
+
+        rmes /= samples as f32;
+        rmes = rmes.sqrt();
+
+        println!("RMES: {}", rmes);
+        assert!(rmes < 1e-3);
+    }
 }
